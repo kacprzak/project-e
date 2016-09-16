22c84e0718beb96f5496ec6eab9fb2cf313e1afa
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-16 15:15:42 +0200

    Removed not used u variable

--- problem_3.hs
+++ problem_3.hs
@@ -3,7 +3,7 @@ divisors n = 1 : filter ((==0) . rem n) [2 .. n `div` 2]
 
 primeFactors :: Int -> [Int]
 primeFactors x = case divisors x of
-  u:d:_ -> d:primeFactors (div x d)
-  u:[]  -> [x]
+  1:d:_ -> d : primeFactors (div x d)
+  1:[]  -> [x]
   
 main = print $ maximum $ primeFactors 600851475143
