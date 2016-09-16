d60ad7337781798a3f9dc8cd2e86915c03eb9045
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-16 16:08:40 +0200

    Update problem_4.hs

--- problem_4.hs
+++ problem_4.hs
@@ -1,4 +1,4 @@
 isPalindrome :: String -> Bool
 isPalindrome w = w == reverse w
 
-main = print $ maximum $ filter (isPalindrome . show) [x*y | x <- [999,998..100], y <- [x-1,x-2..100]]
+main = print $ maximum $ filter (isPalindrome . show) [x*y | x <- [999,998..100], y <- [x,x-1..100]]
