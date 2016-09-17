c3338e831b10511cb8018f3601006e22c91df59e
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-17 13:46:23 +0200

    formula for sum

--- problem_6.hs
+++ problem_6.hs
@@ -3,7 +3,7 @@ sumOfSqrs n = sum $ [x*x | x <- [1..n]]
 
 sqrOfSum :: Integer -> Integer
 sqrOfSum n = s*s
-  where s = sum [1..n]
+  where s = (n*(n+1)) `div` 2
 
 main = print $ (sqrOfSum n) - (sumOfSqrs n)
   where n = 100
