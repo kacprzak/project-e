d21b3b940876b040ba7ab89782b709c082626f8a
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-17 20:08:40 +0200

    Problem 10

---
+++ problem_10.hs
@@ -0,0 +1,9 @@
+primes :: [Integer]
+primes = 1:2:3:prs
+  where
+    1:p:candidates = [6*k+r | k <- [0..], r <- [1,5]]
+    prs            = p : filter isPrime candidates
+    isPrime n      = all (not . divides n) $ takeWhile (\p -> p*p <= n) prs
+    divides n p    = n `rem` p == 0
+
+main = print $ sum $ takeWhile (<2000000) $ tail primes
