178a82bc825c835203703f13810802000d154d94
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-17 16:17:31 +0200

    Problem 9 brute force

---
+++ problem_9.hs
@@ -0,0 +1,7 @@
+pithagoreanTriplets = [(a,b,c) | c <- [1..], b <- [1..c-1], a <- [1..b-1], a*a+b*b==c*c]
+
+main = print $ prod $ head $ filter eq1000 pithagoreanTriplets
+  where
+    eq1000 (a,b,c) = a+b+c==1000
+    prod (a,b,c) = a*b*c
+
