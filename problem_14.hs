22afe31031a85e509c305d2acce03ebcf884ec0a
Marcin Kacprzak <martin.kacprzak@gmail.com>
2016-09-28 12:22:07 +0200

    Solved problem 14

--- problem_14.hs
+++ problem_14.hs
@@ -1,10 +1,20 @@
+import Data.List
+import Data.Ord
+
+takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
+takeWhileInclusive _ [] = []
+takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs
+                                         else []
+
 collatzNext :: Int -> Int
 collatzNext n
   | even n = div n 2
   | odd n  = 3*n + 1
 
 collatz :: Int -> [Int]
-collatz 1 = [1]
-collatz n = n : collatz (collatzNext n)
+collatz = iterate collatzNext
 
-main = print $ maximum $ map (length . collatz) [1..999999] 
+collatzFinite :: Int -> [Int]
+collatzFinite n = takeWhileInclusive (/=1) $ collatz n
+
+main = print $ fst $ maximumBy (comparing snd) $ map (\x -> (x, (length . collatzFinite) x)) [1..999999] 
