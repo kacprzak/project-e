c7e77566660f83ecba567c5809200443d4a27857
Marcin Kacprzak <martin.kacprzak@gmail.com>
2017-06-07 19:13:41 +0200

    faster p025

--- p025.hs
+++ p025.hs
@@ -1,7 +1,3 @@
-digs :: Integral a => a -> [a]
-digs 0 = []
-digs x = digs (x `div` 10) ++ [x `mod` 10]
-
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-main = print $ (+1) $ fst $ last $ zipWith (,) [0..] (takeWhile (<1000) $ map (length . digs) fibs)
+main = print $ (+1) $ fst $ last $ zipWith (,) [0..] (takeWhile (<10^999) $ fibs)
