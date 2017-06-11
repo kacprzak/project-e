0fc882321260f98d702dd231d1999803fc35ca06
Marcin Kacprzak <martin.kacprzak@gmail.com>
2017-06-11 19:41:38 +0200

    Problem 24

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 SOURCES=*.hs
-TARGETS=p001 p002 p003 p004 p005 p006 p007 p008 p009 p010 p011 p012 p013 p014 p015 p016 p018 p020 p025 p048 p067
+TARGETS=p001 p002 p003 p004 p005 p006 p007 p008 p009 p010 p011 p012 p013 p014 p015 p016 p018 p020 p024 p025 p048 p067
 
 all: $(TARGETS)
+
+%: %.cpp
+	clang++ -std=c++11 -o $@ $<
 
 %: %.hs
 	ghc --make $@
---
+++ p024.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+template <typename T> bool next_permutation(std::vector<T> &array) {
+  // Find non-increasing suffix
+  if (array.size() == 0)
+    return false;
+  typename std::vector<T>::iterator i = array.end() - 1;
+  while (i > array.begin() && *(i - 1) >= *i)
+    --i;
+  if (i == array.begin())
+    return false;
+
+  // Find successor to pivot
+  typename std::vector<T>::iterator j = array.end() - 1;
+  while (*j <= *(i - 1))
+    --j;
+  std::iter_swap(i - 1, j);
+
+  // Reverse suffix
+  std::reverse(i, array.end());
+  return true;
+}
+
+int main() {
+  std::vector<int> data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (int i = 1; i < 1000000; ++i)
+    next_permutation(data);
+
+  for (auto x : data)
+    std::cout << x;
+  std::cout << '\n';
+}
